603d0e0bbe5e0a534a753403d8e35faa9b21ceb4
tubatasnim <tasnimtuba22@gmail.com>
2022-03-01 09:26:46 +0600

    second-commit

--- phone.js
+++ phone.js
@@ -13,4 +13,25 @@ const searchPhone = () => {
 const displaySearchResult = data => {
     // console.log(data);
     const searchResult = document.getElementById('search-result');
+    data.forEach(data => {
+        console.log(data);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = ` 
+        <div class="card">
+             <img src="${data.image}" class="card-img-top" alt="...">
+             <div class="card-body">
+                <h4 class="card-title">${data.phone_name}</h4>
+                <h5 class="card-title">${data.brand}</h5>
+                <p class="card-text"></p>
+             </div>
+        </div>
+        `;
+
+        searchResult.appendChild(div);
+    })
+}
+const phoneDetail = () => {
+    console.log('detail clicked');
+
 }
