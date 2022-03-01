2534ea258fda09a1c0e480f5c096cdfb86cc4587
tubatasnim <tasnimtuba22@gmail.com>
2022-03-01 17:25:12 +0600

    fourth commit

--- phone.js
+++ phone.js
@@ -30,7 +30,7 @@ const displaySearchResult = data => {
     }
 
     data.forEach(data => {
-        console.log(data);
+        // console.log(data);
         const div = document.createElement('div');
         div.classList.add('col');
         div.classList.add('d-flex');
@@ -41,64 +41,99 @@ const displaySearchResult = data => {
             <div class="card-body">
               <h4 class="card-title">${data.phone_name}</h4>
               <h5 class="card-title">${data.brand}</h5>
-              <a onclick="phoneDetails('${data.data}')" class="btn btn-primary">Detail</a>
+              <a onclick="loadPhoneDetails ('${data.slug}')" class="btn btn-primary">Detail</a>
             </div>
           </div>
            `;
         searchResult.appendChild(div);
     })
-}
-const loadPhoneDetails = async phoneId => {
+};
+const loadPhoneDetails = phoneId => {
     // console.log(phoneId);
-    const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`
-    const res = await fetch(url)
-    const data = await res.json();
-    displayPhoneIdDetail(data.data.slug)
-    // .then(res => res.json())
-    // .then(data => displayIdDetail(data.data))
-
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayPhoneIdDetail(data.data))
 }
-const displayPhoneIdDetail = id => {
-    console.log(id);
-    // const phoneDetail = document.getElementById('phone-details');
-    // phoneDetail.textContent = '';
-    // const div = document.createElement('div');
-    // div.classList.add('card');
-    // div.innerHTML = `
-    // <img src="${data.image}" class="card-img-top" alt="...">
-    // <div class="card-body">
-    //   <h4 class="card-title">${data.phone_name}</h4>
-
-    //   <h5 class="card-title">${data.brand}</h5>
-    //   <h5 class="card-title">${data.data.slug}')</h5>
-    //   <a onclick="loadPhoneDetails(phoneId)" class="btn btn-primary">Detail</a>
-    // </div>
-    // `;
-    // phoneDetail.appendChild(div);
+const displayPhoneIdDetail = data => {
+    // console.log(data);
+    const detailsDisplay = document.getElementById('phone-details');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <img src="${data.image}" class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">${data.slug}</h5>
+    <h5 class="card-title">${data.mainFeatures.storage}</h5>
+    <h5 class="card-title">${data.mainFeatures.displaySize}</h5>
+    <h5 class="card-title">${data.mainFeatures.chipSet}</h5>
+    <h5 class="card-title">${data.mainFeatures.memory}</h5>
+    <h5 class="card-title">${data.mainFeatures.sensors['']}</h5>
+    <h5 class="card-title">${data.mainFeatures.others}</h5>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+    `;
+    detailsDisplay.appendChild(div);
 }
-// const clickButton = () => {
-//     const url = `https://openapi.programming-hero.com/api/phone/${id}`
-//     // console.log(url);
-//     fetch(url)
-//         .then(res => res.json())
-//         .then(data => displaySearchResult(data.data))
+
+ // <h5 class="card-title">${data.mainFeatures}</h5>
+        // <h5 class="card-title">${data.mainFeatures}</h5>
+        
+        // <h5 class="card-text">${data.mainFeatures.sensors[0]}</h5>
+        // <h5 class="card-text">${data.mainFeatures.others}</h5>
+
+
+// const loadPhoneDetails = async phoneId => {
+//     // console.log(phoneId);
+//     const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`
+//     const res = await fetch(url)
+//     const data = await res.json();
+//     displayPhoneIdDetail(data.data.slug)
+//     // .then(res => res.json())
+//     // .then(data => displayIdDetail(data.data))
 
 // }
-// const displayDetailResult = data => {
-//     // console.log(data);
-//     const detailResult = document.getElementById('detail-result');
-//     data.forEach(data => {
-//         const div = document.createElement('div');
-//         // div.classList.add('col');
-//         // div.classList.add('d-flex');
-//         // div.classList.add('justify-content-around');
-//         div.innerHTML = `
-//               <h4 class="card-title">${data.mainFeatures.sensors}</h4>
-//               <h5 class="card-title">${data.mainFeatures.others}</h5>
-//            `;
-//         detailResult.appendChild(div);
+// const displayPhoneIdDetail = id => {
+//     console.log(id);
+//     // const phoneDetail = document.getElementById('phone-details');
+//     // phoneDetail.textContent = '';
+//     // const div = document.createElement('div');
+//     // div.classList.add('card');
+//     // div.innerHTML = `
+//     // <img src="${data.image}" class="card-img-top" alt="...">
+//     // <div class="card-body">
+//     //   <h4 class="card-title">${data.phone_name}</h4>
 
-
-//     })
+//     //   <h5 class="card-title">${data.brand}</h5>
+//     //   <h5 class="card-title">${data.data.slug}')</h5>
+//     //   <a onclick="loadPhoneDetails(phoneId)" class="btn btn-primary">Detail</a>
+//     // </div>
+//     // `;
+//     // phoneDetail.appendChild(div);
 // }
-{/* <h6 class="card-title">${data.mainFeatures.releaseDate}</h6> */ }
+// // const clickButton = () => {
+// //     const url = `https://openapi.programming-hero.com/api/phone/${id}`
+// //     // console.log(url);
+// //     fetch(url)
+// //         .then(res => res.json())
+// //         .then(data => displaySearchResult(data.data))
+
+// // }
+// // const displayDetailResult = data => {
+// //     // console.log(data);
+// //     const detailResult = document.getElementById('detail-result');
+// //     data.forEach(data => {
+// //         const div = document.createElement('div');
+// //         // div.classList.add('col');
+// //         // div.classList.add('d-flex');
+// //         // div.classList.add('justify-content-around');
+// //         div.innerHTML = `
+// //               <h4 class="card-title">${data.mainFeatures.sensors}</h4>
+// //               <h5 class="card-title">${data.mainFeatures.others}</h5>
+// //            `;
+// //         detailResult.appendChild(div);
+
+
+// //     })
+// // }
+// {/* <h6 class="card-title">${data.mainFeatures.releaseDate}</h6> */ }
